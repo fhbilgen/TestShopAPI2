d004cbf166d3d7d0fa18f955f9fe70a2f2f8b6f0
Faik Hakan Bilgen <faikb@microsoft.com>
2025-07-27 13:37:08 +0300

    Added DoubleHopIncreaseMemory to call a memory consuming API in the remote system

--- TestShopAPI2/Controllers/OuterRim.cs
+++ TestShopAPI2/Controllers/OuterRim.cs
@@ -7,6 +7,13 @@ namespace TestShopAPI2.Controllers
     [ApiController]
     public class OuterRim : ControllerBase
     {
+        private static List<string> _messages = new List<string>
+        {
+            "Outer Rim is a remote area of the galaxy, far from the core worlds.",
+            "It is known for its lawlessness and the presence of various alien species.",
+            "The Outer Rim Territories are often explored by smugglers and adventurers.",
+            "Many planets in the Outer Rim are rich in resources but lack proper governance."
+        };
         private readonly ILogger<OuterRim> _logger;
         private static Random r = new Random();
 
@@ -29,6 +36,21 @@ namespace TestShopAPI2.Controllers
             System.Threading.Thread.Sleep(i);
             
             return i;
+        }
+
+        [HttpGet]
+        public ActionResult<int> GrowTheStory()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                _messages.Add("A new story has been added to the Outer Rim lore.");
+                _messages.Add("The Outer Rim is becoming a hub for intergalactic trade.");
+                _messages.Add("Adventurers are flocking to the Outer Rim in search of fame and fortune.");
+                _messages.Add("The Outer Rim is home to many mysterious and ancient artifacts.");
+            }
+
+            return _messages.Count;
+
         }
     }
 
